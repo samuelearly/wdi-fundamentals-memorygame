cf750aeefb39547c1d93a6ee6727dfd57ab2f8f1
Samuel Early <samuelearly@samuels-macbook.attlocal.net>
2019-06-09 23:38:38 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -35,13 +35,29 @@ function checkForMatch() {
   }
 };
 
-function flipCard(cardId) { 
+function flipCard() { 
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank); 
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(3);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+};
+
+
+createBoard();
+
+
+
