42ffec4a384aa03fae974710cb49545c5b90946d
Samuel Early <samuelearly@samuels-macbook.attlocal.net>
2019-06-09 18:58:55 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 console.log("Up and running!");
+
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[3];
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");}	
-    else 
-    	{alert ("Sorry, try again.")}
-}
+		console.log("You found a match!");
+	} else {
+    	console.log("Sorry, try again.");
+    }
+  }
+};
+
+function flipCard(cardId) { 
+	console.log("User flipped " + cards[cardId]); 
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(3);
